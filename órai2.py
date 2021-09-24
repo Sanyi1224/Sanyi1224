f0dcad7077ecb6fcb6ea1ca3f7f70d071e4a127e
Sanyi1224 <j3315js@gmail.com>
2021-09-24 14:47:02 +0200

    Add files via upload

---
+++ órai2.py
@@ -0,0 +1,9 @@
+number = 10
+if number < 10:
+    print("Ez a szám kisebb, mint 10.")
+else:
+    print("Ez a szám nagyobb vagy egyenlő 10-zel.")
+if number > 100:
+    print("Ez a szám nagyobb, mint 100.")
+else:
+    print("Ez a szám nem nagyobb, mint 100.")
