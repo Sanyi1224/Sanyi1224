2c10c22597b755cbd6d6ea3060259e4619e4df87
Sanyi1224 <j3315js@gmail.com>
2021-11-30 17:36:16 +0100

    Delete hónapok.py

--- hónapok.py
+++
@@ -1,7 +0,0 @@
-days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-month = ['január', 'február', 'március', 'április', 'május', 'június', 'július', 'augusztus', 'szeptember', 'október', 'november', 'december']
-month_and_days = []
-for i in range(len(month)):
-    month_and_days.append(month[i])
-    month_and_days.append(days_in_month[1])
-print(month_and_days)
