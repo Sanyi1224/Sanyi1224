93d075cd5c09df4e54afd4efeb5a6a6d95647496
Sanyi1224 <j3315js@gmail.com>
2021-11-30 17:57:47 +0100

    .

--- circle_perimeter_and_area.py
+++
@@ -1,3 +0,0 @@
-import math
-print(2 * 12 * math.pi)
-print(12*12*math.pi)
--- hf3_2.py
+++
@@ -1,5 +0,0 @@
-num = float(input('Adja meg a termék árát: '))
-if num < 10000:
-    print(num*0.9, "10%")
-else:
-    print(num*0.8, "20%")
