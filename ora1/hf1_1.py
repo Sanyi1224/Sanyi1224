f758e5fae6f95a3e7601d3743a40831f8e0d1ea6
Sándor <j3315js@gmail.com>
2021-12-02 15:30:46 +0100

    .

--- ora1/hf1_1.py
+++ ora1/hf1_1.py
@@ -1,14 +1,14 @@
 # A megoldas, Python2-ben is mukodik
-print("Kinga")
+print("Sándor")
 print("Windows 10")
 print("pte_ep_c2_2021")
 
 print("\n")
 
 # B megoldas, csak Python3-ban mukodik
-print("Kinga", "Windows 10", "pte_ep_c2_2021", sep='\n')
+print("Sándor", "Windows 10", "pte_ep_c2_2021", sep='\n')
 
 print("\n")
 
 # C megoldas, Python2-ben is mukodik
-print("Kinga\nWindows 10\npte_ep_c2_2021")
+print("Sándor\nWindows 10\npte_ep_c2_2021")
