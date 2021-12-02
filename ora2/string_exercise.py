0454d66df6656bb4b006066af8be887aba777c19
Sándor <j3315js@gmail.com>
2021-12-02 16:55:51 +0100

    .

--- ora2/string_exercise.py
+++ ora2/string_exercise.py
@@ -11,3 +11,23 @@ str5 = "programozás"
 str6 = str4 + " " + str5
 print(str4, str5)
 print(str6)
+str7 = "péntek van"
+print ((str7[0:6] + " ")* 7)
+str8 = "hegy"
+str9 = "tananyag"
+if str8 in str3:
+    print("A hegy szó benne van str3-ban.")
+if str8 not in str3:
+    print("A hegy szó nincs benne str3-ban.")
+if str9 in str3:
+    print("A tananyag szó benne van str3-ban.")
+if str9 not in str3:
+    print("A tananyag szó nincs benne str3-ban.")
+str10 = "alma"
+str11 = "Alma"
+if str10 == str11:
+    print("Azonosak.")
+else:
+    print("Nem azonosak.")
+eredmeny = str10 > str11
+print(eredmeny)
